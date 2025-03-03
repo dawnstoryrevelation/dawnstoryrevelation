285c7e30a1fa6b0b088215753fcf79226146828f
dawnstoryrevelation <musicaljasperj@gmail.com>
2025-03-03 22:08:39 +1100

    huh

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
-  // Use a relative path that works better for different deployment environments
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'assets',
   devServer: {
-    historyApiFallback: true,
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/$/, to: '/app.html' },
+        { from: /./, to: '/app.html' }
+      ]
+    },
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
@@ -13,12 +17,8 @@ module.exports = {
       }
     }
   },
-  // This ensures that the index.html file will be properly created
-  // And all assets will be properly referenced
   chainWebpack: config => {
-    // This helps with SPA routing in production
     if (process.env.NODE_ENV === 'production') {
-      // Copy the 404.html file to the output directory
       config.plugin('copy').tap(args => {
         args[0].push({
           from: 'public/404.html',
